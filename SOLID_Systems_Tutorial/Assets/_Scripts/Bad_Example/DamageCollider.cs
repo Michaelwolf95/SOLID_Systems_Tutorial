485d62115e1694651eb35b66c74d976b75babe71
Michael Wolf <michaelwolf95@gmail.com>
2019-05-24 18:20:38 -0700

    "Bad Example" classes.

---
+++ SOLID_Systems_Tutorial/Assets/_Scripts/Bad_Example/DamageCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SOLID_Tutorial.Bad_Example
+{
+    // Example of BAD coding practices (part 3)
+    // This script deals damage to BOTH the Player and Enemies.
+    public class DamageCollider : MonoBehaviour
+    {
+        public float damageValue = 25f;
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                PlayerHealth hp = collision.gameObject.GetComponent<PlayerHealth>();
+                hp.ApplyDamage(damageValue);
+            }
+            else if (collision.gameObject.tag == "Enemy")
+            {
+                EnemyHealth hp = collision.gameObject.GetComponent<EnemyHealth>();
+                hp.ApplyDamage(damageValue);
+            }
+        }
+    }
+}
---
+++ SOLID_Systems_Tutorial/Assets/_Scripts/Bad_Example/PlayerHealth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOLID_Tutorial.Bad_Example
+{
+    // Example of BAD coding practices (part 1)
+    public class PlayerHealth : MonoBehaviour
+    {
+        public float health = 100f;
+        private bool isDead = false;
+
+        public void ApplyDamage(float damage)
+        {
+            if (isDead) return;
+
+            health -= damage;
+
+            if (health <= 0)
+            {
+                health = 0;
+                isDead = true;
+                DoDeathAnimation();
+
+                RestartLevel();
+            }
+            else
+                DoTakeDamageAnimation();
+
+            UpdateHealthBar();
+        }
+
+        #region Dummy Methods
+        private void DoDeathAnimation()
+        {
+
+        }
+
+        private void RestartLevel()
+        {
+
+        }
+
+        private void DoTakeDamageAnimation()
+        {
+
+        }
+
+        private void UpdateHealthBar()
+        {
+
+        }
+
+        #endregion
+    }
+}
